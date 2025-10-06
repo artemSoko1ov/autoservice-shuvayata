a82ebaa442a7ba051fb7f18996b0dea18bbe6414
Artem Sokolov <the.jelll.2006@gmail.com>
2025-10-06 19:08:08 +0500

    basic structure

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/helpers/index.scss" as *;`,
+      },
+    },
+  },
+});
